c4ff1b0e45084c51782c09d6a7daabc62a21790d
Askaredox <andyecarvajal@gmail.com>
2022-04-26 13:08:47 -0600

    backend template

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,39 @@ class Login(Resource):
     def get(self):
         return {'ok': True}
 
-@api.resource('/Rack', '/Rack/<int:id>')
+
+@api.resource('/usuario', '/usuario/<int:id>')
+class Usuario(Resource):
+    def get(self, id):
+        return {'ok': True}
+
+    def post(self):
+        content = request.json
+        necessary_data = [
+            'carnet',
+            'nombre'
+        ]
+        if(all(name in content.keys() for name in necessary_data)):
+            return {'ok': True}, 200
+        else:
+            return {'status': 1, 'msg':'Data no válida'}, 400
+
+    def put(self, id):
+        content = request.json
+        necessary_data = [
+            'carnet',
+            'nombre'
+        ]
+        if(all(name in content.keys() for name in necessary_data)):
+            return {'ok': True}, 200
+        else:
+            return {'status': 1, 'msg':'Data no válida'}, 400
+
+    def delete(self, id):
+        return {'ok': True}
+
+
+@api.resource('/rack', '/rack/<int:id>')
 class Rack(Resource):
     def get(self, id):
         return {'ok': True}
@@ -52,7 +84,7 @@ class Rack(Resource):
         return {'ok': True}
 
 
-@api.resource('/Batea', '/Batea/<int:id>')
+@api.resource('/batea', '/batea/<int:id>')
 class Batea(Resource):
     def get(self, id):
         return {'ok': True}
@@ -81,6 +113,23 @@ class Batea(Resource):
 
     def delete(self, id):
         return {'ok': True}
+
+
+@api.resource('/historial', '/historial/<int:id>')
+class Historial(Resource):
+    def get(self):
+        return {'ok': True}
+
+    def post(self):
+        content = request.json
+        necessary_data = [
+            'carnet',
+            'nombre'
+        ]
+        if(all(name in content.keys() for name in necessary_data)):
+            return {'ok': True}, 200
+        else:
+            return {'status': 1, 'msg':'Data no válida'}, 400
 
 
 if __name__ == '__main__':
